15faa7bf937da4fc88745d71719702dd831ce06a
Tan Nguyen <crazytan94@gmail.com>
2019-05-13 00:51:39 +0700

    Makre sure database created

---
+++ Entities/DbCollection.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Entities
+{
+	public static class DbCollection
+	{
+		private const string EntitiesNamespace = "Entities.Entities";
+
+		public static IEnumerable<string> All()
+		{
+			return Assembly.GetExecutingAssembly()
+				.GetTypes()
+				.Where(_ => _.IsClass && _.Namespace == EntitiesNamespace)
+				.Select(_ => _.Name)
+				.ToList();
+		}
+	}
+}
---
+++ Entities/Migrations/AppMigration.cs
@@ -0,0 +1,43 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Threading.Tasks;
+
+namespace Entities.Migrations
+{
+	public static class AppMigration
+	{
+		public static async Task MakeSureDatabaseCreated(IMongoClient mongoDbClient)
+		{
+			try
+			{
+				var database = mongoDbClient.GetDatabase("BookStoreDB");
+
+				using (var session = await mongoDbClient.StartSessionAsync())
+				{
+					var collections = DbCollection.All();
+
+					foreach (var collectionName in collections)
+					{
+						if (await database.ExistsCollection(collectionName))
+							continue;
+
+						await database.CreateCollectionAsync(session, collectionName);
+					}
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e);
+				throw;
+			}
+		}
+
+		private static async Task<bool> ExistsCollection(this IMongoDatabase database, string collectionName)
+		{
+			var filter = new BsonDocument("name", collectionName);
+			var collectionCursor = await database.ListCollectionsAsync(new ListCollectionsOptions { Filter = filter });
+			return await collectionCursor.AnyAsync();
+		}
+	}
+}

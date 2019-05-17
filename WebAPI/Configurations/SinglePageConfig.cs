99477012f0fbde0a22f0ae91eba42eaf0ad0e924
Tan Nguyen <crazytan94@gmail.com>
2019-05-18 00:49:46 +0700

    Add note for Angular intergration

--- WebAPI/Configurations/SinglePageConfig.cs
+++ WebAPI/Configurations/SinglePageConfig.cs
@@ -9,13 +9,16 @@ namespace WebAPI.Configurations
 		{
 			//
 			// Should not re-order codes
+			// TODO: Make sure the package Microsoft.AspNetCore.StaticFiles has installed
 			app.Use(async (context, next) =>
 			{
 				await next();
 
 				// If there's no available file and the request doesn't contain an extension, we're probably trying to access a page.
 				// Rewrite request to use app root
-				if (context.Response.StatusCode == 404 && !Path.HasExtension(context.Request.Path.Value) && !context.Request.Path.Value.StartsWith("/api"))
+				if (context.Response.StatusCode == 404
+					&& !Path.HasExtension(context.Request.Path.Value)
+					&& !context.Request.Path.Value.StartsWith("/api"))
 				{
 					context.Request.Path = "/index.html";
 					context.Response.StatusCode = 200; // Make sure we update the status code, otherwise it returns 404
